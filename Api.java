e6227c043afb834134198a64452d99ead5f3c8f4
Pankuri <pankuri.bansal@apisero.com>
2021-08-13 11:29:43 +0530

    7th Commit

--- Api.java
+++
@@ -1,8 +0,0 @@
-public class API
-{
-public static void main(String []args)
-{
-System.out.println("API this side");
-System.out.println("Hi");
-}
-}
--- Demo.java
+++ Demo.java
@@ -3,6 +3,7 @@ public class Demo
 public static void main(String []args)
 {
 System.out.println("Pankuri this side");
+System.out.println("API");
 System.out.println("Hi");
 }
 }
